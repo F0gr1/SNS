e1228d58fc18e730d3c8469074dddce95e0f90f0
F0gr1 <ishigami0314@icloud.com>
2024-02-26 08:08:58 +0900

    Login Components create

--- front/src/app/Components/LoginFormComponent.tsx
+++ front/src/app/Components/LoginFormComponent.tsx
@@ -1,18 +1,25 @@
-import { Input, TextField } from "@mui/material";
+import { Button, Input, TextField } from "@mui/material";
 import React from "react";
+import style from "../module/login.module.css";
 
 const LoginFormComponent = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <Input placeholder="Email" />
-        </div>
-        <div>
-          <Input placeholder="Password" />
+    <form className={style.container}>
+      <div className={style.formCard}>
+        <div className={style.form}>
+          <div>
+            <Input placeholder="Email" fullWidth color="primary" />
+          </div>
+          <div>
+            <Input placeholder="Password" fullWidth color="primary" />
+          </div>
+          <div className={style.formButtonGroup}>
+            <Button variant="contained">新規登録</Button>
+            <Button variant="contained">ログイン</Button>
+          </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
