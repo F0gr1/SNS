47ab13de2430442341b97bb00536fc131e9141ac
F0gr1 <ishigami0314@icloud.com>
2024-02-26 08:08:39 +0900

    Header Components create

---
+++ front/src/app/Components/HeaderComponent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import style from "../module/header.module.css";
+const HeaderComponent = () => {
+  return <header className={style.header}>User</header>;
+};
+
+export default HeaderComponent;
